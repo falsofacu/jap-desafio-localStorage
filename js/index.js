1bce32e2f7fe27a5f7ecf856b1194c19be757375
falsofacu <facundotabarez0@gmail.com>
2023-08-21 10:29:34 -0300

    resolved

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const inputElement = document.getElementById("inputText");
+  const textButton = document.getElementById("buttonText");
+
+  textButton.addEventListener("click", () => {
+    localStorage.setItem("data", inputElement.value);
+  })
+})
